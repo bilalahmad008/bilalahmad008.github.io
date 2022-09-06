d026716ae97a47989ffee16b99f120e686d6e349
bilalahmad008 <112877393+bilalahmad008@users.noreply.github.com>
2022-09-06 13:40:19 +0500

    Delete console directory

--- console/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path 
-from console import views
-
-urlpatterns = [
-    path("",views.index,name="home"),
-]
--- console/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.http.response import HttpResponseRedirect
-
-# Create your views here.
-
-def index(request):
-    return render(request, "console/index.html")
-
